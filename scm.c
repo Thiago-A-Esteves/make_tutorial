9522d9cc6af00687313a9c343183a38be8af08a1
Thiago-A-Esteves <121464376+Thiago-A-Esteves@users.noreply.github.com>
2025-02-24 11:49:40 -0300

    Update scm.c
    Update code

--- scm.c
+++ scm.c
@@ -8,9 +8,40 @@ void multiplica(int a, int b) {
     printf("%d x %d = %d\n", a, b, a * b);
 }
 
+void menu() {
+    int opcao, x, y;
+    
+    do {
+        printf("\nEscolha uma operação:\n");
+        printf("1 - Somar\n");
+        printf("2 - Multiplicar\n");
+        printf("0 - Sair\n");
+        printf("Opção: ");
+        scanf("%d", &opcao);
+
+        if (opcao == 1 || opcao == 2) {
+            printf("Digite dois números: ");
+            scanf("%d %d", &x, &y);
+        }
+
+        switch (opcao) {
+            case 1:
+                soma(x, y);
+                break;
+            case 2:
+                multiplica(x, y);
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (opcao != 0);
+}
+
 int main() {
     printf("Olá, Estudante!\n");
-    soma(5, 3);
-    multiplica(5, 3);
+    menu();
     return 0;
 }
